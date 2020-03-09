78cd20cdf6bb0c4c4a83cd0ae606987104b3ef65
Tushar-ml <58730675+Tushar-ml@users.noreply.github.com>
2020-03-09 21:02:41 +0530

    Update main.py

--- M.A.H.I.R.A(Micro Automated Human Intelligence Replica Assistant/main.py
+++ M.A.H.I.R.A(Micro Automated Human Intelligence Replica Assistant/main.py
@@ -1,4 +1,4 @@
-from gtts import gTTS
+import pyttsx3
 from googletrans import Translator
 import speech_recognition as sr 
 from face_recognition_system import Face_Recognition 
@@ -8,8 +8,12 @@ import pyautogui
 import os
 import cv2
 from playsound import playsound
-
-#enter the name of usb microphone that you found 
+import datetime
+import wikipedia
+import webbrowser
+import keyboard
+#enter the name of u
+# sb microphone that you found 
 #using lsusb 
 #the following name is only used as an example 
 mic_name = 'Microsoft Sound Mapper - Input'
@@ -21,7 +25,7 @@ sample_rate = 48000
 chunk_size = 2048
 #Initialize the recognizer 
 r = sr.Recognizer() 
-  
+
 #generate a list of all audio cards/microphones 
 mic_list = sr.Microphone.list_microphone_names() 
   
@@ -40,104 +44,113 @@ def jarvis():
     #wait for a second to let the recognizer adjust the  
     #energy threshold based on the surrounding noise level 
         # print("Speak to Search")
+        r.energy_threshold = 300
+        r.pause_threshold = 1
         r.adjust_for_ambient_noise(source) 
         
         #listens for the user's input 
         audio = r.listen(source) 
               
         try: 
-            text = r.recognize_google(audio) 
+            text = r.recognize_google(audio)
+            print('You said : {}'.format(text))
+            text = text.lower()
+            return text
         except sr.UnknownValueError: 
-                text = 'Not recognized'
-                          
+                
+                print('Speak again')
+                jarvis()
         except sr.RequestError as e: 
                 print('Speak Again')
                 jarvis()
-    return text
+                
+    
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('rate',150)
+engine.setProperty('voice',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+def wishMe(author):
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak('Good Morning {} Sir'.format(author))
+    elif hour>=12 and hour<16:
+        speak('Good Afternoon {} Sir'.format(author))
+    else:
+        speak('Good Evening {} Sir'.format(author))
+            
+
 def Open_Camera():
-      name = Face_Recognition()
-      if name=='Tushar':
-          print('Welcome {}'.format(name))
+    cap = cv2.VideoCapture(0)
+    while True:
+        _,frame = cap.read()
+        cv2.imshow('frame',frame)
+        
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
           
 print('Say Something')
-with sr.Microphone(device_index = device_id, sample_rate = sample_rate,  
-                        chunk_size = chunk_size) as source: 
-    #wait for a second to let the recognizer adjust the  
-    #energy threshold based on the surrounding noise level 
-        r.adjust_for_ambient_noise(source) 
-        
-        #listens for the user's input 
-        audio = r.listen(source) 
-              
-        try: 
-            text = r.recognize_google(audio) 
-            print ("you said: " + text)
-            if text.lower() == 'start':
-                '''speech = gTTS(text = 'Hi Tushar How could i help you', lang = 'en', slow = False)
-                
-                speech.save('reply.mp3')
-                playsound('reply.mp3')'''
-                
-                while True:
-                    with sr.Microphone(device_index = device_id, sample_rate = sample_rate,  
-                                        chunk_size = chunk_size) as source: 
-                    #wait for a second to let the recognizer adjust the  
-                    #energy threshold based on the surrounding noise level 
-                        r.adjust_for_ambient_noise(source) 
-                        
-                        #listens for the user's input 
-                        audio = r.listen(source)
-                              
-                        try: 
-                            text = r.recognize_google(audio) 
-                            print ("you said: " + text)
+if __name__=='__main__':
+                        #
+                        # wishMe('Tushar')
+                        speak('Welcome')
+                        speak('Wait for a Moment , Identifying you , will not take a minute ')
                             
-                            word = text.split()
-                            if word[0].lower() == 'open' and word[1].lower() == 'firefox':
-                                print(word)
-                                search_words = word[4:]
-                                os.system('start firefox.exe')
-                                time.sleep(10)
-                                a = ''
-                                for i in range(len(search_words)):
-                                    a = a+' '+search_words[i]
+                        name = Face_Recognition()
+                        if name=='Tushar':
+                              wishMe(name)
+                              speak('What can i do for you Sir')
+                              while True:
+                                
+                                text = jarvis()
+                                
+                               
+                                
                                 try:
-                                        
-                                            time.sleep(2)
-                                            pyautogui.hotkey('ctrl','k')
-                                            pyautogui.typewrite(a)
-                                            pyautogui.press('enter')
-                                except TimeoutException:
-                                        print ("Loading took too much time!")
+                                    if 'entertain' or 'bored' or 'tired' in text:
+                                        speak("what would you like to have Sir: Listening Music or Watching Videos")
+                                        entertain_text = jarvis()
+                                        if 'video' or 'videos' or 'watching videos' in entertain_text:
+                                            speak('Opening Youtube')
+                                            webbrowser.open('www.youtube.com')
+                                            speak('Waiting for the youtube to load')
+                                            time.sleep(15)
+                                            speak('What you would you like to Tushar Sir')
+                                            search_text = jarvis()
+                                            if search_text:
+                                                pyautogui.press('tab')
+                                                keyboard.write(str(search_text))
                                     
-                            if text.lower() == 'open camera':
-                                print('Opening the Web Camera')
-                                Open_Camera()
-                              
-                            if text=='stop':
-                                print('Stopping Procedure')
-                                break
+                                    if 'open' and 'camera' in text:
+                                        print('Opening the Web Camera')
+                                        speak('Opening the Camera')
+                                        Open_Camera()
+                                      
+                                    if 'stop' in text:
+                                        print('Stopping Procedure')
+                                        speak('Successfully Stopped. Have a Good day Sir')
+                                        break
+                                    if 'translate' in text:
+                                        speak('What to Translate Sir')
+                                        translated_speech = jarvis()
+                                        translator = Translator()
                                     
-                            '''translator = Translator()
-                        
-                            translated = translator.translate(str(text),src='en',dest='hi')
-                        
-                            tts = gTTS(text='Successfully Stopped', lang='hi')
-                            tts.save("good.mp3")
-                            from playsound import playsound
-                            playsound('good.mp3')'''
-                      
-                    #error occurs when google could not understand what was said 
-                      
-                        except sr.UnknownValueError: 
-                            continue
+                                        translated = translator.translate(translated_speech,src='hi',dest='en')
+                                        trans = str(translated.text)
+                                        speak(trans)
+                                  
+                                except:
+                                    speak('Having Some Problem')
+                                    speak('Sorry for the Inconvienence')
+                        else:
+                            speak('Access Denied')
+                            
                           
-                        except sr.RequestError as e: 
-                            continue
-            else:
-                print("Invalid Command")
-        except sr.UnknownValueError: 
-                print("Not Recognized")
-                          
-        except sr.RequestError as e: 
-                print('Not Recognized')
+                 
